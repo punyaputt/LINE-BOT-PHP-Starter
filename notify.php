eb962de6da83d70a14d3b2a815cdc5803729c28c
punyaputt <fubao.deo@gmail.com>
2017-04-10 12:27:28 +0700

    Update notify.php

--- notify.php
+++ notify.php
@@ -2,7 +2,7 @@
 define('LINE_API',"https://notify-api.line.me/api/notify");
  //oHsGoAFIBFIoUHn2Gio8UuTrnv6EtuUYaWM7STkebTm mytoken
  //wcyEAm1ppXmTosJbJnzChSz3won2MBNNkFxdv9RHkK7 secure group token
-$token = "wcyEAm1ppXmTosJbJnzChSz3won2MBNNkFxdv9RHkK7"; //ใส่Token ที่copy เอาไว้
+$token = "oHsGoAFIBFIoUHn2Gio8UuTrnv6EtuUYaWM7STkebTm"; //ใส่Token ที่copy เอาไว้
 
 
 $str = "เกิดเหตุฉุกเฉิน โปรดเช็คกล้องที่ http://192.168.0.104:1414"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
