334675a8860518156d63c69dafe08bc488f6badf
punyaputt <fubao.deo@gmail.com>
2020-03-03 20:18:11 +0700

    Update motion.php

--- motion.php
+++ motion.php
@@ -3,13 +3,13 @@ define('LINE_API',"https://notify-api.line.me/api/notify");
  //oHsGoAFIBFIoUHn2Gio8UuTrnv6EtuUYaWM7STkebTm mytoken/
  //wcyEAm1ppXmTosJbJnzChSz3won2MBNNkFxdv9RHkK7 secure group token
 $token = "Xr0dlthqsczXQjQVlcHzRvFCy6qTPiwRNhCEkfx5YyL"; //ใส่Token ที่copy เอาไว้
+$access_token = "Xr0dlthqsczXQjQVlcHzRvFCy6qTPiwRNhCEkfx5YyL";
 $cctv = "http://baokung.ddns.eagleeyes.tw:1414";
 
 
 $str = "detected"; //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
 curl_setopt($curl_handle, CURLOPT_POST, 1);
-$cfile = new CurlFile('temp.jpg');
-curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $cfile);
+$imageFile = 'temp.jpg';
 
  
 $res = notify_message($str,$cfile,$token); //ไม่รันฟังชั่นเลย แต่รันเป้นการแทนตัวแปรเพื่อรับค่ารีเทิร์น
@@ -32,5 +32,30 @@ function notify_message($message,$imageFile,$token){ //รูปแบบลำ�
  $result = file_get_contents(LINE_API,FALSE,$context);
  $res = json_decode($result);
  return $res;
+}
+
+$message_data = array('imageFile' => $imageFile);
+
+function send_notify_message($line_api, $access_token, $message_data){
+   $headers = array('Method: POST', 'Content-type: multipart/form-data', 'Authorization: Bearer '.$access_token );
+
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $line_api);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $message_data);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   $result = curl_exec($ch);
+   // Check Error
+   if(curl_error($ch))
+   {
+      $return_array = array( 'status' => '000: send fail', 'message' => curl_error($ch) );
+   }
+   else
+   {
+      $return_array = json_decode($result, true);
+   }
+   curl_close($ch);
+return $return; //รีเทินไปที่ตัวแปรนั้นๆ
 }
 ?>
